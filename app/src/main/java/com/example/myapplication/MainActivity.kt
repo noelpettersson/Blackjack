d85b219a4738da2b53e202a0455d338c4a891117
noelpettersson <noel.pettersson@iths.se>
2022-09-21 08:08:27 +0200

    Money left text / money bet text

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,13 +2,21 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 var moneyInBank: Int = 500 // The amount of money the player currently have
 var bet: Int = 0 // Variable used for the amount of money in the bet
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val moneyLeftText = findViewById<TextView>(R.id.moneyLeftText)
+        val betAmountText = findViewById<TextView>(R.id.betAmountText)
+
+        moneyLeftText.text = "Money in bank: ${moneyLeftText}"
+        betAmountText.text = "Bet: ${betAmountText}"
     }
 }
